bd09d4e7c77df6677c7b63e2d7e1b5126ba165b0
Kustov-Artyom <artemkust174@gmail.com>
2026-05-05 16:20:22 +0500

    update storage downloaded files

--- server/index.js
+++ server/index.js
@@ -21,7 +21,8 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, '../public/images'));
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
   }
 });
 
